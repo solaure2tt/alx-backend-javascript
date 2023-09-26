662bdf921f19f09baa587c4edbc90c96fd7b0db8
solaure2tt <solaure2t@yahoo.fr>
2023-09-26 18:52:29 +0000

    task 3

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,5 +1,5 @@
-import uploadPhoto from "./utils"
-import createUser from "./utils"
+import { uploadPhoto, createUser } from './utils';
+
 export default function handleProfileSignup() {
   return Promise
     .all([uploadPhoto(), createUser()])
