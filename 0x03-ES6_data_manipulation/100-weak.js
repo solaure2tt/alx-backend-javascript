db87e820ce967235414037e7e6bccfc5678a0789
solaure2tt <solaure2t@yahoo.fr>
2023-10-03 00:00:57 +0000

    update task 11

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -1,13 +1,13 @@
-export const wMap = new WeakMap();
+export const weakMap = new WeakMap();
 
-export const qAPI = (endpoint) => {
-  if (wMap.has(endpoint)) {
-    const endpointData = wMap.get(endpoint);
+export const queryAPI = (endpoint) => {
+  if (weakMap.has(endpoint)) {
+    const endpointData = weakMap.get(endpoint);
     if (endpointData >= 4) {
       throw new Error('Endpoint load is high');
     }
-    wMap.set(endpoint, (endpointData + 1));
+    weakMap.set(endpoint, (endpointData + 1));
   } else {
-    wMap.set(endpoint, 1);
+    weakMap.set(endpoint, 1);
   }
 };
